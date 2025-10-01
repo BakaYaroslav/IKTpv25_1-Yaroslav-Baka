08c081f3c6a96c135dbff1336246b718e2b1c9ff
opilane <opilane@HP-CZC2349HTC>
2025-10-01 11:07:21 +0300

    #Harlutus 1, Ülesanded 1, 2

--- IKTpv25_1_Yaroslav_Baka.py
+++ IKTpv25_1_Yaroslav_Baka.py
@@ -1,1 +1,49 @@
-print ("tere maailm!")
+# 1.
+# Kirjuta enda esimene programm, mis väljastab käsureale teksti: “Tere, maailm!”. 
+# Küsi kasutaja nimi ja muuda tekst, et ta näeks välja nii: “Tere, maailm! Tervitan sind Mati”, kui kasutaja nimi on Mati.
+
+# Küsi kasutajalt sisend tema vanuse kohta ning väljasta see ekraanile:
+
+# “Tere, maailm! Tervitan sind Mati! Sa oled N aastat vana.”
+
+
+
+
+
+# from os import PathLike
+
+
+# print("Tere maailm!")
+# nimi = input("sisesta oma nimi: ").capitalize()
+# print(f"Tere maailm! Tervian sind {nimi}")
+# vanus = int(input("Sisesta oma vanus: "))# konvertib stringi täis arvuks
+# print(f"Tere maailm! Tervian sind {nimi.upper()}. Sa oled {vanus} aastat vana")
+# print(f"Tere maailm! Tervian sind {nimi.lower()}. Sa oled {vanus} aastat vana")
+
+
+# 2
+
+# Mis tüüpi on järgnevad muutujad:
+
+# a) vanus = 18
+# b) eesnimi = "Jaak"
+# c) pikkus = 16.5
+# d) kas_käib_koolis = True
+# Mis võimalus veel peale True oleks viimast muutujat väärtustada? Kuidas võiks nende muutujate väärtusi koodis kontrollida?
+# Kirjuta kood tüüpide kontrollimiseks.
+
+vanus = 18 #int
+eesnimi = "Jaak" #str
+pikkus = 16.5 #float
+kas_kaib_koolis = True #bool
+print(f"vanus {vanus} on: {type(vanus)}")
+print(f"eesnimi {eesnimi} on: {type(eesnimi)}")
+print(f"pikkus {pikkus} on: {type(pikkus)}")
+print(f"kas_käib_koolis {kas_kaib_koolis} on: {type(kas_kaib_koolis)}")
+
+
+
+
+
+
+
