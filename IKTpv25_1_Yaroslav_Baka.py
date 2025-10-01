a4a7f4efc4f8fb2835032edba0ace985e6856667
opilane <opilane@HP-CZC2349HTC>
2025-10-01 11:42:56 +0300

    #üsesanded 1-5.5

--- IKTpv25_1_Yaroslav_Baka.py
+++ IKTpv25_1_Yaroslav_Baka.py
@@ -32,18 +32,53 @@
 # Mis võimalus veel peale True oleks viimast muutujat väärtustada? Kuidas võiks nende muutujate väärtusi koodis kontrollida?
 # Kirjuta kood tüüpide kontrollimiseks.
 
-vanus = 18 #int
-eesnimi = "Jaak" #str
-pikkus = 16.5 #float
-kas_kaib_koolis = True #bool
-print(f"vanus {vanus} on: {type(vanus)}")
-print(f"eesnimi {eesnimi} on: {type(eesnimi)}")
-print(f"pikkus {pikkus} on: {type(pikkus)}") 
-print(f"kas_käib_koolis {kas_kaib_koolis} on: {type(kas_kaib_koolis)}")
+# vanus = 18 #int
+# eesnimi = "Jaak" #str
+# pikkus = 16.5 #float
+# kas_käib_koolis = True #bool
+# print(f"vanus {vanus} on: {type(vanus)}")
+# print(f"eesnimi {eesnimi} on: {type(eesnimi)}")
+# print(f"pikkus {pikkus} on: {type(pikkus)}") 
+# print(f"kas_käib_koolis {kas_käib_koolis} on: {type(kas_käib_koolis)}")
 
 
+# 3
+
+# Kirjuta enda koodis laual olevate kommide arv muutujasse(kommide arv on juhuslik). Seejärel kuva muutujas olev kommide arv ekraanile kasutades print() käsku.
+# Küsi kasutajalt sisendit, mitu kommi ta soovib laualt ära võtta. Eemalda soovitud kommide arv laual olevate kommide arvust ja kuva ekraanile, kui palju komme laual nüüd on.
+
+# from random import *
+# laua_peal = randint(10, 50) #juhuslik arvu 10-50
+# print(f"Laual on {laua_peal} kommi")
+# võtab = int(input("Mitu kommi soovid laualt võtta? ")) #sisend võtab teisendab stringi täisarvuks
+# laua_peal -= võtab #laua_peal = laua_peal - võtab, võtab laualt kommid maha
+# print(f"Nüüd on laual {laua_peal} kommi")
 
 
+# 4
 
+# Puu läbimõõdu arvutamine
+# Kirjuta programm, mis küsib kasutaja käest puu ümbermõõdu ning teatab selle peale puu läbimõõdu.
 
+# from math import *
 
+# C = int(input("Sisesta puu ümbermõõdu cm: "))
+# C= C/pi
+# print(f"Puu ümbermõõt on {C:.2f} cm") # ümardab 2 kohta pärast koma
+
+# 5.
+# Arvutage Pythoni käsureal, kui pikk on ristkülikukujulise maatüki diagonaal, mille mõõtmed on Nm x Mm. N ja M küsi kasutajalt.
+
+# from math import *
+# N = int(input("Sisesta maatüki N külje pikkus: "))
+# M = int(input("Sisesta maatüki M külje pikkus: "))
+# d = sqrt(N**2 + M**2)
+# print(f"Maatüki diagonaal on {d:.2f} ühikut")
+
+# 6
+# Leidke järgnevast näiteprogrammist semantiline viga:
+aeg = int(input("Mitu tundi kulus sõiduks?: ")
+teepikkus = int(input("Mitu kilomeetrit sõitsid? ")
+kiirus = aeg / teepikkus
+
+print(f"Sinu kiirus oli " {str(kiirus)} " km/h")
